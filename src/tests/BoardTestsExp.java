1a533563a5b48de03fe7bb9674322d6e601b9427
Colin Short <colinshort@mymail.mines.edu>
2021-02-24 13:53:16 -0700

    4 Adjacency Tests: Top Left, Bottom Right, Left Edge, Right Edge | 5 Target Calculation Tests: 2 Mixed Scenarios, Empty Board, Occupied Cells, Rooms

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -1,21 +1,21 @@
 package tests;
+import experiment.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.Set;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import experiment.TestBoard;
-import experiment.TestBoardCell;
 
 public class BoardTestsExp {
-	TestBoard board;
+	private TestBoard board;
 	
-	@BeforeEach // Run before each test
+	@Before // Run before each test
 	public void setUp() {
 		board = new TestBoard();
 	}
 	
+	//test adjacency list creation with start at top left
 	@Test
 	public void testAdjTopLeft() {
 		TestBoardCell cell = board.getCell(0,0);
@@ -23,9 +23,10 @@ public class BoardTestsExp {
 		assertTrue(testList.contains(board.getCell(1,0)));
 		assertTrue(testList.contains(board.getCell(0,1)));
 		assertEquals(2, testList.size());
+		
 	}
 	
-	
+	//test adjacency list creation with start at bottom right
 	@Test
 	public void testAdjBottomRight() {
 		TestBoardCell cell = board.getCell(3,3);
@@ -35,6 +36,7 @@ public class BoardTestsExp {
 		assertEquals(2, testList.size());
 	}
 	
+	//test adjacency list creation with start at right edge
 	@Test
 	public void testAdjRightEdge() {
 		TestBoardCell cell = board.getCell(1,3);
@@ -45,6 +47,7 @@ public class BoardTestsExp {
 		assertEquals(3, testList.size());
 	}
 	
+	//test adjacency list creation with start at left edge
 	@Test
 	public void testAdjLeftEdge() {
 		TestBoardCell cell = board.getCell(3,0);
@@ -54,7 +57,100 @@ public class BoardTestsExp {
 		assertEquals(3, testList.size());
 	}
 	
+	/*
+	 * Test target creation with empty board
+	 * start location equals (0,0)
+	 * die roll equals 3
+	 */
+	
+	@Test
+	public void testTargetsNormal() {
+		TestBoardCell cell = board.getCell(0, 0);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		assertEquals(6, targets.size());
+		assertTrue(targets.contains(board.getCell(3, 0)));
+		assertTrue(targets.contains(board.getCell(2, 1)));
+		assertTrue(targets.contains(board.getCell(0, 1)));
+		assertTrue(targets.contains(board.getCell(1, 2)));
+		assertTrue(targets.contains(board.getCell(0, 3)));
+		assertTrue(targets.contains(board.getCell(1, 0)));
+	}
+	
+	/*
+	 * Test target creation with mixed occupied and rooms
+	 * start location equals (0,3)
+	 * die roll equals 3
+	 */
+	@Test
+	public void testTargetsMixed() {
+		board.getCell(0, 2).setOccupied(true);
+		board.getCell(1, 2).setRoom(true);
+		TestBoardCell cell = board.getCell(0,3);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		assertEquals(3, targets.size());
+		assertTrue(targets.contains(board.getCell(1,2)));
+		assertTrue(targets.contains(board.getCell(2, 2)));
+		assertTrue(targets.contains(board.getCell(3, 3)));
+	}
 	
 	
+	/*
+	 * Test target creation with occupied 
+	 * start location equals (2,1)
+	 * die roll equals 4
+	 */
+	@Test
+	public void TestTargetsOccupied() {
+		board.getCell(1, 0).setOccupied(true);
+		board.getCell(1, 1).setOccupied(true);
+		board.getCell(2, 2).setOccupied(true);
+		TestBoardCell cell = board.getCell(2,1);
+		board.calcTargets(cell, 4);
+		Set<TestBoardCell> targets = board.getTargets();
+		assertEquals(2, targets.size());
+		assertTrue(targets.contains(board.getCell(3,2)));
+		assertTrue(targets.contains(board.getCell(2,3)));
+		
+	}
 	
+	/*
+	 * Test target creation with rooms
+	 * start location equals (2,1)
+	 * die roll equals 5
+	 */
+	
+	@Test
+	public void TestTargetsRoom() {
+		board.getCell(1, 1).setRoom(true);
+		board.getCell(2, 0).setRoom(true);
+		board.getCell(3, 1).setRoom(true);
+		board.getCell(2, 2).setRoom(true);
+		TestBoardCell cell = board.getCell(2,1);
+		board.calcTargets(cell, 5);
+		Set<TestBoardCell> targets = board.getTargets();
+		assertEquals(4, targets.size());
+		assertTrue(targets.contains(board.getCell(1,1)));
+		assertTrue(targets.contains(board.getCell(2,0)));
+		assertTrue(targets.contains(board.getCell(3,1)));
+		assertTrue(targets.contains(board.getCell(2,2)));
+	}
+	
+	/* test target creation with rooms and occupied cells
+	 * starting in bottom right corner (3,3),
+	 * blocked on left side (3,2) and a room above (2,3)
+	 * die roll equals 6
+	 */
+	
+	@Test
+	public void TestTargetsMixed2() {
+		board.getCell(3, 2).setOccupied(true);
+		board.getCell(2, 3).setRoom(true);
+		TestBoardCell cell = board.getCell(3, 3);
+		board.calcTargets(cell, 6);
+		Set<TestBoardCell> targets = board.getTargets();
+		assertEquals(1, targets.size());
+		assertTrue(targets.contains(board.getCell(2,3)));
+	}
 }
