6e6b6cf74e42c32339fa72ac1ebb68e1b6e70ef7
Colin Short <colinshort@mymail.mines.edu>
2021-03-22 18:21:12 -0600

    gameSetUpTests comments

--- src/tests/gameSetupTests.java
+++ src/tests/gameSetupTests.java
@@ -19,6 +19,9 @@ class gameSetupTests {
 		board.initialize();
 	}
 
+	//test for correct number of players
+	//test for correct number of human players
+	//test for correct number of computer players
 	@Test
 	public void testPlayers() {
 		ArrayList<Player> myPlayers = board.getPlayers();
@@ -37,6 +40,7 @@ class gameSetupTests {
 		assertEquals(numComputer, 5);
 	}
 	
+	//test for correct number of cards and card types in the deck
 	@Test
 	public void testDeck() {
 		ArrayList <Card> myCards = board.getDeck();
@@ -61,6 +65,7 @@ class gameSetupTests {
 		assertEquals(6, people);
 	}
 	
+	//test that the solution has a person, room, and weapon card
 	@Test
 	public void testSolution() {
 		Solution solution = board.getAnswer();
@@ -69,23 +74,28 @@ class gameSetupTests {
 		assertEquals(CardType.WEAPON, solution.getWeapon().getCardType());
 	}
 	
+	//test for proper deal
 	@Test
 	public void testDeal() {
 		ArrayList<Card> deck = board.getDeck();
 		ArrayList<Player> players = board.getPlayers();
-		int cardsLeft = deck.size() - 3;
-		int min = cardsLeft / players.size();
-		int count = 0;
-		int sum = 0;
+		int cardsLeft = deck.size() - 3; //number of cards not dealt (excludes solution cards)
+		int min = cardsLeft / players.size(); //minimum possible cards per hand
+		int count = 0; //count invalid hands
+		int sum = 0; //sum of number of cards in each hand
 		for(int i = 0; i < players.size(); i++) {
 			ArrayList<Card> myCards = players.get(i).getHand();
 			sum += myCards.size();
+			//check for invalid hand
+			//valid hands either have min or min + 1 cards
 			if(myCards.size() > min + 1 || myCards.size() < min) {
 				count++;
 			}
 		}
+		//check that there are no invalid hands
 		assert(count == 0);
-		assert(sum == deck.size() - 3);
+		//check that correct number of cards were dealt (all cards dealt and no double-dealing)
+		assert(sum == cardsLeft);
 	}
 
 }
