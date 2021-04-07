a2952fe1508482690bb1ba5a4c0f8d3f26684945
cameron-fitz <fitzgerald@mymail.mines.edu>
2021-04-07 16:29:20 -0600

    added ClueGame class

---
+++ src/clueGame/ClueGame.java
@@ -0,0 +1,27 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class ClueGame extends JFrame {
+	private static Board board;
+
+	public ClueGame() {
+		setSize(1000,1000);
+		board = Board.getInstance();
+		board.setPreferredSize(new Dimension(800, 800));
+		board.setConfigFiles("ClueBoard.csv", "ClueSetup.txt");		
+		board.initialize();
+		add(board, BorderLayout.CENTER);
+	}
+	
+	
+	public static void main(String[] args) {
+		ClueGame frame = new ClueGame();
+		frame.setContentPane(board); // put the panel in the frame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
+		frame.setVisible(true); // make it visible
+	}
+}
