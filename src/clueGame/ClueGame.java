8f4a63f697a24e1af15abdfcee1eef5644721746
Colin Short <colinshort@mymail.mines.edu>
2021-04-07 16:46:17 -0600

    Added Known Cards panel and Game Control Panel

--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -7,20 +7,28 @@ import javax.swing.JFrame;
 
 public class ClueGame extends JFrame {
 	private static Board board;
+	private static KnownCardsPanel kcp;
+	private static GameControlPanel gcp;
 
 	public ClueGame() {
-		setSize(1000,1000);
+		//setSize(1000,1000);
 		board = Board.getInstance();
 		board.setPreferredSize(new Dimension(800, 800));
 		board.setConfigFiles("ClueBoard.csv", "ClueSetup.txt");		
 		board.initialize();
 		add(board, BorderLayout.CENTER);
+		
+		kcp = new KnownCardsPanel();
+		add(kcp, BorderLayout.EAST);
+		
+		gcp = new GameControlPanel();
+		add(gcp, BorderLayout.SOUTH);
 	}
 	
 	
 	public static void main(String[] args) {
 		ClueGame frame = new ClueGame();
-		frame.setContentPane(board); // put the panel in the frame
+		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // allow it to close
 		frame.setVisible(true); // make it visible
 	}
